4dcfbdca914194934f45fc8f377b83448f3ca4a2
Jairofontalvo <jairofontalvo@hotmail.es>
2020-03-05 09:57:54 -0500

    Solucion raindrops

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,9 @@
+def convert(number):
+    sonido = ""
+    if number % 3 == 0:
+        sonido += "Pling"
+    if number % 5 == 0:
+        sonido += "Plang"
+    if number % 7 == 0:
+        sonido += "Plong"   
+    return sonido if sonido != "" else str(number)
